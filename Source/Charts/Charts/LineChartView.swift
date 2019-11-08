2a6f2514716b64dba02bfc71644310ee13aced41
Nathanael Anderson <nathan@master-technology.com>
2019-11-07 22:49:20 -0600

    More fixes

--- Source/Charts/Charts/LineChartView.swift
+++ Source/Charts/Charts/LineChartView.swift
@@ -22,11 +22,19 @@ open class LineChartView: BarLineChartViewBase, LineChartDataProvider
         renderer = LineChartRenderer(dataProvider: self, animator: _animator, viewPortHandler: _viewPortHandler)
     }
 
-   @objc open func setData(lineData: LineChartData) {
+   @objc open func setData(_ lineData: LineChartData) {
         data = lineData
    }
 
     // MARK: - LineChartDataProvider
     
-    open var lineData: LineChartData? { return _data as? LineChartData }
+    open var lineData: LineChartData? {
+            get {
+                return _data as? LineChartData
+            }
+            set {
+                 NSLog("NativeScriptData set from LineData")
+                 data = newValue
+            }
+    }
 }
